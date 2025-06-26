7a974b3cb6b89f9a33a56a338e0df4bc3934fb81
felipelifa <felipe.delifa@gmail.com>
2025-06-26 00:05:54 -0300

    att

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// firebase-config.js
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDeZUn-c0Rln-4sNOO0EYDY7lTD4ikK_fA",
+  authDomain: "barbeariatest.firebaseapp.com",
+  projectId: "barbeariatest",
+  storageBucket: "barbeariatest.appspot.com",
+  messagingSenderId: "693253339206",
+  appId: "1:693253339206:android:64e978b91d5bfc2277b247"
+};
+
+firebase.initializeApp(firebaseConfig);
